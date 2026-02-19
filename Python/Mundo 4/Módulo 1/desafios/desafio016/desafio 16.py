44d6ed533f2d2a25bb741e97df9b64ba2fd8bcb7
João Paulo Medeiros Rodrigues <jp.estudos17@gmal.com>
2026-02-19 16:00:25 -0300

    Corrige desafio 16

--- Python/Mundo 4/Módulo 1/desafios/desafio016/desafio 16.py
+++ Python/Mundo 4/Módulo 1/desafios/desafio016/desafio 16.py
@@ -4,16 +4,23 @@ from rich.traceback import install
 install()
 
 
-class funcionario():
-    def __init__(self, nome, setor, cargo, empresa="Curso em Vídeo"):
+class Funcionario():
+    """
+    Cria um funcionário da empresa Curso em Vídeo
+    Para criar um funcionário, faça:
+    f1 = Funcionario(nome,setor,cargo)
+    """
+
+    # Atributos de Classe
+    empresa = "Curso em Vídeo"
+
+    def __init__(self, nome, setor, cargo):
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
-        self.empresa = empresa
 
     def apresentacao(self):
-        printf(
-            f":handshake: Olá, sou [blue]{self.nome}[/] e sou {self.cargo} do setor de {self.setor} da empresa {self.empresa}.")
+        return f":handshake: Olá, sou [blue]{self.nome}[/] e sou {self.cargo} do setor de {self.setor} da empresa {Funcionario.empresa}."
 
     def __getstate__(self):
         tabela = Table(title="Funcionário")
@@ -22,9 +29,17 @@ class funcionario():
         tabela.add_column("[bold]Setor[/]", justify="center", style="Blue")
         tabela.add_column("[bold]Empresa[/]", justify="center", style="Blue")
 
-        tabela.add_row(self.nome, self.cargo, self.setor, self.empresa)
+        tabela.add_row(self.nome, self.cargo,
+                       self.setor, self.empresa)
 
 
-func1 = funcionario("Manu", "Marketing", "Gerente", "SODIÊ")
-func1.apresentacao()
+func1 = Funcionario("Manu", "Marketing", "Gerente")
+func1.empresa = "Hostnet"
+printf(func1.apresentacao())
+
+func2 = Funcionario("João", "TI", "Programador")
+printf(func2.apresentacao())
+
+inspect(Funcionario)
 inspect(func1)
+inspect(func2)
