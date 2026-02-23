b96c2250bf81540dbf40f58c8b693f683cbd6506
João Paulo Medeiros Rodrigues <jp.estudos17@gmal.com>
2026-02-23 18:29:27 -0300

    corrige desafio 17

--- Python/Mundo 4/desafios/resolvidos/desafio_017/main.py
+++ Python/Mundo 4/desafios/resolvidos/desafio_017/main.py
@@ -1,26 +1,23 @@
 from rich import print as printf
 from rich.panel import Panel
-from rich.console import Group
-from rich.text import Text
+from rich.align import Align
 from rich.traceback import install
 install()
 
 
-class produto():
+class Produto():
     def __init__(self, nome=" ", preco=0):
         self.nome = nome
         self.preco = preco
 
     def etiqueta(self):
-
-        mensagem = Group(Text(" "*11, end=""), f"{self.nome}",  # Linha 1
-                         "-"*30,                               # Linha 2
-                         # Linha 3
-                         Text("."*10, end=""), Text(f"R${self.preco:.2f}", end=""), Text("."*11, end=""))
-
-        painel = Panel(mensagem, title=f" Produto ", expand=False)
+        conteudo = f"{self.nome.center(30, ' ')}"
+        conteudo += f"{'-' * 30}"
+        precof = f"R${self.preco:,.2f}"
+        conteudo += f"{precof.center(30, '.')}"
+        painel = Panel(conteudo, title=f" Produto ", width=34)
         printf(painel)
 
 
-celular = produto("Celular", 6000)
+celular = Produto("Iphone 17", 6000)
 celular.etiqueta()
