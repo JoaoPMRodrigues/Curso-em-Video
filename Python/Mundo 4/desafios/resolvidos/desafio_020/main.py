76040d2dc9a3aaa383b8e4093670c5bce3f787b4
João Paulo (Pop-os) <jp.estudos17@gmail.com>
2026-03-19 23:26:22 +0000

    Melhora desafio 20

--- Python/Mundo 4/desafios/resolvidos/desafio_020/main.py
+++ Python/Mundo 4/desafios/resolvidos/desafio_020/main.py
@@ -1,12 +1,18 @@
 from rich import print
 from rich.panel import Panel
 from rich.console import Group
-from rich.text import Text
 from rich.traceback import install
 install()
 
 
 class jogador():
+    """
+    Cria um jogador com seu nick de jogo,
+    adciona jogos favoritos e mostra todos em
+    ordem alfabética.
+    j1 = jogador('nome','nickname')
+    j1.add('jogo1')
+    """
     def __init__(self, nome, nickname):
         self.nome = nome
         self.nick = nickname
@@ -18,22 +24,25 @@ class jogador():
     def ficha(self):
         self.jogos.sort()
         lista_jogos = "\n".join([f"🎮 {jogo}" for jogo in self.jogos])
+        
         if not lista_jogos:
-            lista_jogos = Text(
-                f"{self.nick} não tem jogos favoritos", style="red")
-        mensagem = Group(f"Nome real: [black on blue]{self.nome}[/]",
-                         "Jogos favoritos: ",
-                         lista_jogos)
+            print(f"[red]{self.nick} não tem jogos favoritos[/]")
+            return None
+        
+        mensagem = Group(f"Nome real: [blue]{self.nome}[/]",
+                         "Jogos favoritos: ", lista_jogos)
 
-        painel = Panel(
-            mensagem, title=f"Jogador(a) <{self.nick}>", expand=False)
+        painel = Panel(mensagem, title=f"Jogador(a) <{self.nick}>", expand=False)
         print(painel)
 
 
 j1 = jogador("João Paulo", "Guibisson_912")
-j1.add_favoritos("Clash Royale")
-j1.add_favoritos("Minecraft")
 j1.add_favoritos("Pokemon")
+j1.add_favoritos("Minecraft")
+j1.add_favoritos("Clash Royale")
 j1.ficha()
+
 j2 = jogador("Caio", "Caioba")
 j2.ficha()
+
+
