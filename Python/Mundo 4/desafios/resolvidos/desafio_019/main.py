da806da11b7f912ed5ddf5bde3670b3367680aae
João Paulo (Pop-os) <jp.estudos17@gmail.com>
2026-03-30 18:01:34 -0300

    Corrige desafio 22 e formata desafio 19

--- Python/Mundo 4/desafios/resolvidos/desafio_019/main.py
+++ Python/Mundo 4/desafios/resolvidos/desafio_019/main.py
@@ -8,26 +8,29 @@ class livro():
     def __init__(self, titulo, paginas):
         self.titulo = titulo
         self.paginas = paginas
-        self.posicao = 1  
+        self.posicao = 1
 
-        print(f""":open_book: [blue]Você acabou de abrir o livro [red]{self.titulo}[/] que tem [green]{self.paginas} páginas[/] no total. Você está na [yellow]{self.posicao} página[/][/blue]""")
+        print(
+            f""":open_book: [blue]Você acabou de abrir o livro [red]{self.titulo}[/] que tem [green]{self.paginas} páginas[/] no total. Você está na [yellow]{self.posicao} página[/][/blue]""")
 
     def fim_do_livro(self):
-        return True if self.paginas==self.posicao else False
-    
+        return True if self.paginas == self.posicao else False
+
     def avancar_paginas(self, n):
         numero_avanco = 0
-        
-        while not self.fim_do_livro() and numero_avanco<n:
+
+        while not self.fim_do_livro() and numero_avanco < n:
             numero_avanco += 1
-            self.posicao  += 1
+            self.posicao += 1
             print(f"Pag{self.posicao} :arrow_forward:", end=" ")
             sleep(0.3)
 
-        print(f"[purple]Você avançou {numero_avanco} páginas e está na[/] [yellow]página {self.posicao}[/]")
-        
+        print(
+            f"[purple]Você avançou {numero_avanco} páginas e está na[/] [yellow]página {self.posicao}[/]")
+
         if self.fim_do_livro():
-            print(f":closed_book: [red]Você chegou ao final do livro '{self.titulo}'[/]")
+            print(
+                f":closed_book: [red]Você chegou ao final do livro '{self.titulo}'[/]")
 
 
 l1 = livro("Grokking Algorithms", 20)
